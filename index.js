7277491830ffeaed5c5804ba4aac380559982d04
WTNB1225 <w.swallowsfan@gmail.com>
2023-10-31 11:22:55 +0900

    changed sentence

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const serverlessExpress = require('@vendia/serverless-express')
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello AWS')
+  res.send('Fuck You!')
 })
 
 exports.handler = serverlessExpress({ app })
